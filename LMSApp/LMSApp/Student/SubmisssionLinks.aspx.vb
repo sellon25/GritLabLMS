4c2f6781dbaa20fde51244bf2b741fb67b025650
ndivhuwotheunknown <166398227+ndivhuwotheunknown@users.noreply.github.com>
2024-06-25 00:23:35 +0200

    .

--- LMSApp/LMSApp/Student/SubmisssionLinks.aspx.vb
+++ LMSApp/LMSApp/Student/SubmisssionLinks.aspx.vb
@@ -4,54 +4,37 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         'loadsubmissionlinks()
     End Sub
-    'Private Function getLinks(student_id As String)
-    '    Dim filter As String = "WHERE userId = '" & student_id & "'"
-    '    Dim submissionlink As List(Of Course_Submission) = New Course_Submission().listall(filter)
-    '    Dim studentlinks As New Course_Submission()
+    Private Sub loadsubmissionlinks()
+        Dim courseId As String = Request.QueryString("courseId")
 
-    '    'For Each enrollment As Question_Bank In projectEnrollments
-    '    '    Dim project As Project = project.load(enrollment.project_id)
-    '    '    If project IsNot Nothing Then
-    '    '        project.name = Trim(project.name)
-    '    '        facilitatorProjects.Add(project)
-    '    '    End If
-    '    'Next
+        Dim submissionlinks_ As List(Of Course_Submission) = GetSubmissionlinks(courseId)
 
-    '    Return facilitatorProjects
-    'End Function
-    'Protected Sub loadsubmissionlinks()
-    '    Dim student_id As String = Session("ID").ToString()
+        For Each link As Course_Submission In submissionlinks_
+            Dim submissionlink As Submission = GetSubmissionlink(link.submission_id)
+            SubmissionLinks.Controls.Add(CreateCourseHtml(submissionlink))
+        Next link
 
-    '    Dim submissionlinks As Question_Bank
-    '    submissionlinks = New Question_Bank
-    '    Dim links As List(Of Question_Bank) = listall(student_id)
-    '    ' Fetch submission links 
-    '    ' Dim submissionlinks As List(Of Question_Bank) = GetSubmissionlinks(student_id)
-    '    'Dim projects As List(Of Project) = GetProjectsByFacilitator(student_id)
 
-    '    ' Bind links
-    '    'For Each submissionlink As Question_Bank In Courses
-    '    '    Dim link As New HtmlAnchor()
-    '    '    link.HRef = "CoursePage.aspx?courseId=" & Course.id
-    '    '    link.Attributes("class") = "col-md-3"
+    End Sub
+    Private Function GetSubmissionlink(submission_id) As Submission
+        Dim filter As String = "WHERE id = '" & submission_id & "'"
+        Dim submissionlink As Submission = submissionlink.load(filter)
+        Return submissionlink
+    End Function
+    Private Function GetSubmissionlinks(courseId As String) As List(Of Course_Submission)
+        Dim Filter As String = "WHERE course_id = '" & courseId & "' and userId = '" & Session("userId").ToString() & "'"
 
-    '    '    Dim courseBox As New HtmlGenericControl("div")
-    '    '    courseBox.Attributes("class") = "white-box boxShadow coursebox"
-    '    '    courseBox.Style("background-image") = "url(" & GetImageUrl(Course.thumbnail) & ")"
+        Dim links As List(Of Course_Submission)
 
-    '    '    Dim nameLabel As New Label()
-    '    '    nameLabel.Attributes("class") = "box-title"
-    '    '    nameLabel.Text = Course.name
 
-    '    '    Dim descriptionLabel As New Label()
-    '    '    descriptionLabel.Attributes("class") = "text-muted"
-    '    '    descriptionLabel.Text = Course.description
+        Return links
+    End Function
 
-    '    '    courseBox.Controls.Add(nameLabel)
-    '    '    courseBox.Controls.Add(descriptionLabel)
-    '    '    link.Controls.Add(courseBox)
+    Private Function CreateCourseHtml(course As Submission) As HtmlAnchor
+        Dim link As New HtmlAnchor
+        link.HRef = "SubmitForm.aspx?courseId=" & course.id
 
-    '    '    CoursesRepeater.Controls.Add(link)
-    '    'Next
-    'End Sub
+
+        Return link
+    End Function
 End Class
