5453a39371c81bdd0891ab1fdc9d4f7fed4ef432
sellon25 <117732516+sellon25@users.noreply.github.com>
2024-06-27 15:35:48 +0200

    .

--- LMSApp/LMSApp/SignUp.aspx.vb
+++ LMSApp/LMSApp/SignUp.aspx.vb
@@ -47,7 +47,15 @@ Public Class SignUp
     Protected Function AddQuestionHtml(questionId As String, questionType As String, questionText As String) As HtmlGenericControl
         ' Create a new HtmlGenericControl representing a <div> element
         Dim newQuestionDiv As New HtmlGenericControl("div")
-        newQuestionDiv.Attributes("class") = "form-group mb-4 border-bottom"
+        newQuestionDiv.Attributes("class") = "form-group mb-4"
+
+        ' Create the delete button
+        Dim btnDelete As New Button()
+        btnDelete.ID = String.Format("btnDelete_{0}", questionId)
+        btnDelete.Text = "Delete"
+        btnDelete.CssClass = "btn-0 border-0 text-danger bg-none float-end"
+        AddHandler btnDelete.Click, AddressOf DeleteQuestion
+        btnDelete.EnableViewState = True
 
         ' Create the label for the question text
         Dim lblQuestionText As New Label()
@@ -55,32 +63,32 @@ Public Class SignUp
         lblQuestionText.Text = questionText
 
         ' Add the delete button and label for question text to the newQuestionDiv
+        newQuestionDiv.Controls.Add(btnDelete)
         newQuestionDiv.Controls.Add(lblQuestionText)
 
         ' Add additional controls based on question type
         Select Case questionType
             Case "radio"
                 ' Add radio button inputs and additional controls for radio type questions
-                Dim radioInput1 As New HtmlGenericControl("input")
-                radioInput1.Attributes("type") = "radio"
-                radioInput1.Attributes("name") = questionId
-                radioInput1.Attributes("value") = "Option 1"
+                Dim radioInput1 As New RadioButton()
+                radioInput1.ID = String.Format("radioInput1_{0}", questionId)
+                radioInput1.GroupName = questionId
+                radioInput1.Text = "Option 1"
 
-                Dim radioInput2 As New HtmlGenericControl("input")
-                radioInput2.Attributes("type") = "radio"
-                radioInput2.Attributes("name") = questionId
-                radioInput2.Attributes("value") = "Option 2"
+                Dim radioInput2 As New RadioButton()
+                radioInput2.ID = String.Format("radioInput2_{0}", questionId)
+                radioInput2.GroupName = questionId
+                radioInput2.Text = "Option 2"
 
                 ' Add radio inputs to newQuestionDiv
                 newQuestionDiv.Controls.Add(radioInput1)
                 newQuestionDiv.Controls.Add(radioInput2)
 
                 ' Add an input and button for radio type questions
-                Dim radioTextInput As New HtmlGenericControl("input")
-                radioTextInput.Attributes("id") = "rinp-" & questionId
-                radioTextInput.Attributes("type") = "text"
+                Dim radioTextInput As New TextBox()
+                radioTextInput.ID = "rinp-" & questionId
                 radioTextInput.Attributes("placeholder") = "Type here..."
-                radioTextInput.Attributes("class") = "form-control p-0 border-0"
+                radioTextInput.CssClass = "form-control p-0 border-0"
 
                 Dim addButton As New Button()
                 addButton.ID = "btnAddOp_" + questionId
@@ -96,19 +104,17 @@ Public Class SignUp
                 newQuestionDiv.Controls.Add(addButton)
 
                 ' Add border-bottom class to the div for styling
-                'newQuestionDiv.Attributes("class") &= " border-bottom"
+                newQuestionDiv.Attributes("class") &= " border-bottom"
 
             Case "checkbox"
                 ' Add checkbox inputs for checkbox type questions
-                Dim checkboxInput1 As New HtmlGenericControl("input")
-                checkboxInput1.Attributes("type") = "checkbox"
-                checkboxInput1.Attributes("name") = questionId
-                checkboxInput1.Attributes("value") = "Option 1"
+                Dim checkboxInput1 As New CheckBox()
+                checkboxInput1.ID = String.Format("checkboxInput1_{0}", questionId)
+                checkboxInput1.Text = "Option 1"
 
-                Dim checkboxInput2 As New HtmlGenericControl("input")
-                checkboxInput2.Attributes("type") = "checkbox"
-                checkboxInput2.Attributes("name") = questionId
-                checkboxInput2.Attributes("value") = "Option 2"
+                Dim checkboxInput2 As New CheckBox()
+                checkboxInput2.ID = String.Format("checkboxInput2_{0}", questionId)
+                checkboxInput2.Text = "Option 2"
 
                 ' Add checkbox inputs to newQuestionDiv
                 newQuestionDiv.Controls.Add(checkboxInput1)
@@ -116,38 +122,37 @@ Public Class SignUp
 
             Case "text"
                 ' Add input for text type questions
-                Dim textInput As New HtmlGenericControl("input")
-                textInput.ID = String.Format("textInput_{0}", questionId)
-                textInput.Attributes("runat") = "server"
-                textInput.Attributes("type") = "text"
-                textInput.Attributes("placeholder") = "Type here..."
-                textInput.Attributes("class") = "form-control p-0 border-0"
-
-                ' Add input to newQuestionDiv
-                newQuestionDiv.Controls.Add(textInput)
-                AnswerControls.Add(textInput.ID)
-                ' Add border-bottom class to the div for styling
-                'newQuestionDiv.Attributes("class") &= " border-bottom"
-
-            Case "textarea"
-                ' Add textarea for textarea type questions
                 Dim textInput As New TextBox()
                 textInput.ID = String.Format("textInput_{0}", questionId)
-                textInput.Attributes("runat") = "server"
                 textInput.Attributes("placeholder") = "Type here..."
                 textInput.CssClass = "form-control p-0 border-0"
 
                 ' Add input to newQuestionDiv
                 newQuestionDiv.Controls.Add(textInput)
-                AnswerControls.Add(textInput.ID)
 
                 ' Add border-bottom class to the div for styling
-                'newQuestionDiv.Attributes("class") &= " border-bottom"
+                newQuestionDiv.Attributes("class") &= " border-bottom"
+
+            Case "textarea"
+                ' Add textarea for textarea type questions
+                Dim textareaInput As New TextBox()
+                textareaInput.ID = String.Format("textareaInput_{0}", questionId)
+                textareaInput.TextMode = TextBoxMode.MultiLine
+                textareaInput.Rows = 4
+                textareaInput.Attributes("placeholder") = "Type here..."
+                textareaInput.CssClass = "form-control p-0 border-0"
+
+                ' Add textarea to newQuestionDiv
+                newQuestionDiv.Controls.Add(textareaInput)
+
+                ' Add border-bottom class to the div for styling
+                newQuestionDiv.Attributes("class") &= " border-bottom"
 
             Case "dropList"
                 ' Add select dropdown for dropList type questions
-                Dim selectDropdown As New DropDownList
-                selectDropdown.Attributes("class") = "form-select shadow-none p-0 border-0 form-control-line"
+                Dim selectDropdown As New DropDownList()
+                selectDropdown.ID = String.Format("selectDropdown_{0}", questionId)
+                selectDropdown.CssClass = "form-select shadow-none p-0 border-0 form-control-line"
 
                 ' Add options to select dropdown
                 Dim option1 As New ListItem("Game development")
@@ -159,33 +164,32 @@ Public Class SignUp
                 selectDropdown.Items.Add(option3)
                 selectDropdown.Items.Add(option4)
 
-
                 ' Add select dropdown to newQuestionDiv
                 newQuestionDiv.Controls.Add(selectDropdown)
 
                 ' Add border-bottom class to the div for styling
-                'newQuestionDiv.Attributes("class") &= " border-bottom"
+                newQuestionDiv.Attributes("class") &= " border-bottom"
 
             Case "number"
                 ' Add input for number type questions
-                Dim numberInput As New HtmlGenericControl("input")
+                Dim numberInput As New TextBox()
+                numberInput.ID = String.Format("numberInput_{0}", questionId)
                 numberInput.Attributes("type") = "number"
                 numberInput.Attributes("min") = "0"
                 numberInput.Attributes("max") = "10"
                 numberInput.Attributes("placeholder") = "Type here..."
-                numberInput.Attributes("class") = "form-control p-0 border-0"
+                numberInput.CssClass = "form-control p-0 border-0"
 
                 ' Add input to newQuestionDiv
                 newQuestionDiv.Controls.Add(numberInput)
 
                 ' Add border-bottom class to the div for styling
-                'newQuestionDiv.Attributes("class") &= " border-bottom"
+                newQuestionDiv.Attributes("class") &= " border-bottom"
         End Select
 
         ' Return the div containing the question content
         Return newQuestionDiv
     End Function
-
 
     Protected Sub DeleteQuestion(sender As Object, e As EventArgs)
         Dim btn As Button = DirectCast(sender, Button)
@@ -207,22 +211,37 @@ Public Class SignUp
 
         For Each controlID In AnswerControls
             ' Find the control by its ID
-            Dim textInput As TextBox = CType(FindControl(controlID), TextBox)
+            Dim control As Control = FindControl(controlID)
 
-            If textInput IsNot Nothing Then
-                ' Get the entered text value
-                Dim enteredText As String = textInput.Text
-
-                ' Collect the answer
-                collectedAnswers.Add(controlID, enteredText)
+            If control IsNot Nothing Then
+                If TypeOf control Is TextBox Then
+                    Dim textInput As TextBox = CType(control, TextBox)
+                    Dim enteredText As String = textInput.Text
+                    collectedAnswers.Add(controlID, enteredText)
+                ElseIf TypeOf control Is RadioButton Then
+                    Dim radioInput As RadioButton = CType(control, RadioButton)
+                    If radioInput.Checked Then
+                        collectedAnswers.Add(controlID, radioInput.Text)
+                    End If
+                ElseIf TypeOf control Is CheckBox Then
+                    Dim checkboxInput As CheckBox = CType(control, CheckBox)
+                    If checkboxInput.Checked Then
+                        collectedAnswers.Add(controlID, checkboxInput.Text)
+                    End If
+                ElseIf TypeOf control Is DropDownList Then
+                    Dim dropdownInput As DropDownList = CType(control, DropDownList)
+                    Dim selectedValue As String = dropdownInput.SelectedValue
+                    collectedAnswers.Add(controlID, selectedValue)
+                End If
             End If
         Next
 
-
+        ' Process the collected answers
         For Each kvp In collectedAnswers
             Dim controlID As String = kvp.Key
             Dim answer As String = kvp.Value
             ' Save or process the answer
         Next
     End Sub
+
 End Class
