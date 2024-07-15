0cbb7649c0edd272972111b1d4b454c0f999d502
sellon25 <117732516+sellon25@users.noreply.github.com>
2024-07-15 15:05:55 +0200

    .

--- LMSApp/LMSApp/Utility/SendEmail.vb
+++ LMSApp/LMSApp/Utility/SendEmail.vb
@@ -21,22 +21,96 @@ Public Class SendEmail
         Smtp_Server.Send(e_mail)
     End Sub
 
-    Public Sub SendOTP(Email)
+    Public Sub SendNotification(userId As String, message As String)
+
+
+
+        Dim User As User = New User().load(userId)
+
         Dim Smtp_Server As New SmtpClient
         Dim e_mail As New MailMessage()
+        Dim sb As New StringBuilder
         Smtp_Server.UseDefaultCredentials = False
-        Smtp_Server.Credentials = New Net.NetworkCredential("chietawebapp@gmail.com", "ygigfqjsqysumxvh")
+        Smtp_Server.Credentials = New Net.NetworkCredential("chietawebapp@gmail.com", "madfhywjgtqhwuqd")
         Smtp_Server.Port = 587
         Smtp_Server.EnableSsl = True
         Smtp_Server.Host = "smtp.gmail.com"
 
         e_mail = New MailMessage()
         e_mail.From = New MailAddress("chietawebapp@gmail.com")
-        e_mail.To.Add(Email)
-        e_mail.Subject = "Password Reset"
-        e_mail.IsBodyHtml = False
-        e_mail.Body = "Your password for Account: " + Email + " has been reset successfully" + Environment.NewLine + Environment.NewLine + "Kind regards" + Environment.NewLine + "Chieta Team"
-        'Goto OTP Page
+        e_mail.To.Add("sellondaba25@gmail.com")
+
+        e_mail.Subject = "Grits Lab App Notification"
+
+        e_mail.IsBodyHtml = True
+        Dim link = "https://ulink.uj.ac.za/Default"
+        Dim logoPath As String = "https://gritlabafrica.org/wp-content/uploads/2023/12/screenshot-2023-12-05-064309.png"
+        Dim Body1 As String = "<html>" & vbCrLf &
+            "<body style=""font-family: Arial, sans-serif; text-align: center; background-color: #f4f4f4; padding: 20px;"">" & vbCrLf &
+            "    <div style=""background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"">" & vbCrLf &
+            "        <img src=""" & logoPath & """ alt=""Logo"" style=""width: 200px; height: auto; margin-bottom: 20px;""><br>" & vbCrLf &
+            $"        <h4 style=""color: #333; margin-bottom: 20px;"">Notification from Grits Lab Africa</h2>" & vbCrLf &
+            $"        <p>Dear {User.FName} {User.LName},</p>" & vbCrLf &
+            "        <p>We trust this email finds you well.</p>" & vbCrLf &
+            $"        <p>You have received a new notification on the Grits Lab app. To read it, please log in through Ulink:</p>" & vbCrLf &
+            $"        <div style=""background-color: #93761e; color: #fff; padding: 15px; border-radius: 5px; margin: 15px 0; display: inline-block;"">{message}</div>" & vbCrLf &
+            "    <p><a href=""" & link & """style=""background-color: #93761e; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; margin-top: 10px; margin-bottom: 10px;"">Read</a></p>" & vbCrLf &
+            "        <p>If you have any questions or concerns, please don't hesitate to contact us.</p>" & vbCrLf &
+            "        <p>Thank you for your participation.</p>" & vbCrLf &
+            "        <p>Sincerely,<br>Grits Lab App</p>" & vbCrLf &
+            "    </div>" & vbCrLf &
+            "</body>" & vbCrLf &
+        "</html>"
+
+
+
+        e_mail.Body = Body1
+        Smtp_Server.Send(e_mail)
+    End Sub
+
+
+    Public Sub SendNotification(userId As String, subject As String, message As String)
+
+        Dim User As User = New User().load(userId)
+
+        Dim Smtp_Server As New SmtpClient
+        Dim e_mail As New MailMessage()
+        Dim sb As New StringBuilder
+        Smtp_Server.UseDefaultCredentials = False
+        Smtp_Server.Credentials = New Net.NetworkCredential("chietawebapp@gmail.com", "madfhywjgtqhwuqd")
+        Smtp_Server.Port = 587
+        Smtp_Server.EnableSsl = True
+        Smtp_Server.Host = "smtp.gmail.com"
+
+        e_mail = New MailMessage()
+        e_mail.From = New MailAddress("chietawebapp@gmail.com")
+        e_mail.To.Add("sellondaba25@gmail.com")
+
+        e_mail.Subject = subject
+
+        e_mail.IsBodyHtml = True
+        Dim link = "https://ulink.uj.ac.za/Default"
+        Dim logoPath As String = "https://gritlabafrica.org/wp-content/uploads/2023/12/screenshot-2023-12-05-064309.png"
+        Dim Body1 As String = "<html>" & vbCrLf &
+            "<body style=""font-family: Arial, sans-serif; text-align: center; background-color: #f4f4f4; padding: 20px;"">" & vbCrLf &
+            "    <div style=""background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);"">" & vbCrLf &
+            "        <img src=""" & logoPath & """ alt=""Logo"" style=""width: 200px; height: auto; margin-bottom: 20px;""><br>" & vbCrLf &
+            $"        <h4 style=""color: #333; margin-bottom: 20px;"">Notification from Grits Lab Africa</h2>" & vbCrLf &
+            $"        <p>Dear {User.FName} {User.LName},</p>" & vbCrLf &
+            "        <p>We trust this email finds you well.</p>" & vbCrLf &
+            $"        <p>You have received a new notification on the Grits Lab app. To read it, please log in through Ulink:</p>" & vbCrLf &
+            $"        <div style=""background-color: #93761e; color: #fff; padding: 15px; border-radius: 5px; margin: 15px 0; display: inline-block;"">{message}</div>" & vbCrLf &
+            "    <p><a href=""" & link & """style=""background-color: #93761e; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; margin-top: 10px; margin-bottom: 10px;"">Read</a></p>" & vbCrLf &
+            "        <p>If you have any questions or concerns, please don't hesitate to contact us.</p>" & vbCrLf &
+            "        <p>Thank you for your participation.</p>" & vbCrLf &
+            "        <p>Sincerely,<br>Grits Lab App</p>" & vbCrLf &
+            "    </div>" & vbCrLf &
+            "</body>" & vbCrLf &
+        "</html>"
+
+
+
+        e_mail.Body = Body1
         Smtp_Server.Send(e_mail)
     End Sub
 End Class
